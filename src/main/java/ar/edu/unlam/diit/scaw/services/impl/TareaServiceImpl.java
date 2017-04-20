a165cffefd932b3e9769558289ae09dc4ffd3aa2
julianbermolen <julianbermolen@gmail.com>
2017-04-20 13:00:37 -0300

    Tarea y listado

---
+++ src/main/java/ar/edu/unlam/diit/scaw/services/impl/TareaServiceImpl.java
@@ -0,0 +1,35 @@
+package ar.edu.unlam.diit.scaw.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.unlam.diit.scaw.services.TareaService;
+import ar.edu.unlam.diit.scaw.daos.TareaDao;
+import ar.edu.unlam.diit.scaw.entities.Tarea;
+
+
+
+public class TareaServiceImpl implements TareaService {
+	@Autowired
+	TareaDao tareaDao;
+	
+	@Override
+	public void save(Tarea tarea) {
+		tareaDao.save(tarea);
+	}
+
+	@Override
+	public List<Tarea> findAll() {
+		return tareaDao.findAll();
+	}
+
+	public TareaDao getTareaDao() {
+		return tareaDao;
+	}
+
+	public void setTareaDao(TareaDao tareaDao) {
+		this.tareaDao = tareaDao;
+	}
+	
+}
